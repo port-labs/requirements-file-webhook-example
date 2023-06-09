212cf65d904eb9bda012e4be8503a6d095facfe6
pages_coffy <isaac.p.coffie@gmail.com>
2023-06-09 12:55:20 +0000

    align blueprint relation and remove unused code

--- python-tutorial/ingest.py
+++ python-tutorial/ingest.py
@@ -28,41 +28,6 @@ def add_entity_to_port(entity_object):
     return response.json()
 
 
-def convert_package_json(package_json_path):
-    """This function takes a package.json file path, converts the "dependencies" property into a 
-    JSON array using three keys (name, version, and id). It then sends this data to Port
-
-    Params
-    --------------
-    package_json_path: str
-        The path to the package.json file relative to the project's root folder
-    
-    Returns
-    --------------
-    response: dict
-        The response object after calling the webhook
-    """
-    with open(package_json_path) as file:
-        data = json.load(file)
-
-    dependencies = data.get('dependencies', {})
-
-    converted_dependencies = []
-    for index, (name, version) in enumerate(dependencies.items(), start=1):
-        pkg_id = f"pkg-{index}"
-        converted_dependencies.append({
-            'name': name,
-            'version': version,
-            'id': pkg_id
-        })
-
-    entity_object = {
-        "service": SERVICE_ID,
-        "dependencies": converted_dependencies
-    }
-    webhook_response = add_entity_to_port(entity_object)
-    return webhook_response
-
 def convert_requirements_txt(requirements_txt_path):
     """This function takes a requirements.txt file path, converts all the dependencies into a 
     JSON array using three keys (name, version, and id). It then sends this data to Port
